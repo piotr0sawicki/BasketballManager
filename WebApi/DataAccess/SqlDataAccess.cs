cf6c324deb5f8426bac79ece773e14106e1c02ae
Piotr Sawicki <01151556@pw.edu.pl>
2021-09-08 21:54:46 +0200

    Clean up SqlDataAccess file

--- WebApi/DataAccess/SqlDataAccess.cs
+++ WebApi/DataAccess/SqlDataAccess.cs
@@ -13,6 +13,13 @@ namespace WebApi.Library.DataAccess
 {
     public class SqlDataAccess : IDisposable, ISqlDataAccess
     {
+        private IDbConnection _connection;
+        private IDbTransaction _transaction;
+        private bool isClosed = false;
+        private readonly IConfiguration _config;
+        private readonly ILogger<SqlDataAccess> _logger;
+
+
         public SqlDataAccess(IConfiguration config, ILogger<SqlDataAccess> logger)
         {
             _config = config;
@@ -21,7 +28,6 @@ namespace WebApi.Library.DataAccess
         public string GetConnectionString(string name)
         {
             return _config.GetConnectionString(name);
-            //return ConfigurationManager.ConnectionStrings[name].ConnectionString; 
         }
 
         public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
@@ -46,9 +52,6 @@ namespace WebApi.Library.DataAccess
             }
         }
 
-        private IDbConnection _connection;
-        private IDbTransaction _transaction;
-
         public void StartTransaction(string connectionStringName)
         {
             string connectionString = GetConnectionString(connectionStringName);
@@ -71,10 +74,6 @@ namespace WebApi.Library.DataAccess
             List<T> rows = _connection.Query<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure, transaction: _transaction).ToList();
             return rows;
         }
-
-        private bool isClosed = false;
-        private readonly IConfiguration _config;
-        private readonly ILogger<SqlDataAccess> _logger;
 
         public void CommitTransaction()
         {
